dc7370b4c7e7c7a60806626825da5b0e7d9aa50a
Cesar Caetano Neto <cesar.neto@zlabels.de>
2017-08-02 15:04:15 +0200

    prepareDatabase impl

---
+++ src/main/java/io/cesarcneto/vertx/guide/wiki/MainVerticle.java
@@ -0,0 +1,69 @@
+package io.cesarcneto.vertx.guide.wiki;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+/**
+ * Created by ccaetanoneto on 02.08.17.
+ */
+public class MainVerticle extends AbstractVerticle {
+
+  private static final String SQL_CREATE_PAGES_TABLE = "create table if not exists Pages (Id integer identity primary key, Name varchar(255) unique, Content clob)";
+  private static final String SQL_GET_PAGE = "select Id, Content from Pages where Name = ?";
+  private static final String SQL_CREATE_PAGE = "insert into Pages values (NULL, ?, ?)";
+  private static final String SQL_SAVE_PAGE = "update Pages set Content = ? where Id = ?";
+  private static final String SQL_ALL_PAGES = "select Name from Pages";
+  private static final String SQL_DELETE_PAGE = "delete from Pages where Id = ?";
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
+
+  private JDBCClient dbClient;
+
+  @Override
+  public void start(Future<Void> startFuture) throws Exception {
+    Future<Void> steps = prepareDatabase().compose(v -> startHttpServer());
+    steps.setHandler(ar -> {
+      if (ar.succeeded()) {
+        startFuture.complete();
+      } else {
+        startFuture.fail(ar.cause());
+      }
+    });
+  }
+
+  private Future<Void> prepareDatabase() {
+    Future<Void> future = Future.future();
+
+    dbClient = JDBCClient.createShared(vertx, new JsonObject()
+      .put("url", "jdbc:hsqldb:file:db/wiki")
+      .put("driver_class", "org.hsqldb.jdbcDriver")
+      .put("max_pool_size", 30)
+    );
+
+    dbClient.getConnection(ar -> {
+      if (ar.failed()) {
+        LOGGER.error("Could not open a database connection", ar.cause());
+        future.fail(ar.cause());
+      } else {
+        SQLConnection connection = ar.result();
+        connection.execute(SQL_CREATE_PAGES_TABLE, create -> {
+          connection.close();
+          if (create.failed()) {
+            LOGGER.error("Database preparation error", create.cause());
+            future.fail(create.cause());
+          } else {
+            future.complete();
+          }
+        });
+      }
+    });
+    return future;
+  }
+
+  private Future<Void> startHttpServer() {
+    Future<Void> future = Future.future();
+    // (...)
+    return future;
+  }
+
+}
