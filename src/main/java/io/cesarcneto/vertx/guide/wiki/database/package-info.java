2b291df946596dbd3fa398026482c3ea891fd36a
Cesar Caetano Neto <cesar.neto@zlabels.de>
2017-08-03 15:40:42 +0200

    refactoring to vertx services

---
+++ src/main/java/io/cesarcneto/vertx/guide/wiki/database/package-info.java
@@ -0,0 +1,4 @@
+@ModuleGen(groupPackage = "io.cesarcneto.vertx.guide.wiki.database", name = "wiki-database")
+package io.cesarcneto.vertx.guide.wiki.database;
+
+import io.vertx.codegen.annotations.ModuleGen;
